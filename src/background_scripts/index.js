325e62a89e4b9176d2948936448901d41d7d0c3e
iknowhtml <iknowsmtp@gmail.com>
2020-04-29 20:35:25 -0400

    Change from CommonJS to module import

--- src/background_scripts/index.js
+++ src/background_scripts/index.js
@@ -1,6 +1,6 @@
-const leftPad = require("left-pad");
+import leftPad from 'left-pad';
 
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    const result = leftPad(message.text, message.amount, message.with);
-    sendResponse(result);
+  const result = leftPad(message.text, message.amount, message.with);
+  sendResponse(result);
 });
